a00d89124a54e68079b45a2b7d9ffb88d1994646
computer_chris <computergeek99@gmail.com>
2023-08-11 21:55:14 -0700

    Implemented random cursor color

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-const container = document.querySelector('#container');
-const hoverClass = 'hover';
-const defaultGridSize = 16;
-
-// Create header
-const divhdr = document.createElement('div');
-container.appendChild(divhdr);
-// Create header button
-const hdrbtn = document.createElement('button');
-divhdr.appendChild(hdrbtn);
-hdrbtn.textContent = "Click to Adjust Grid Size";
-// Header button click action
-hdrbtn.addEventListener('click', function() {
-    let newGridSize = prompt("Grid size - inputer number between 2 and 100", defaultGridSize);
-    if(isNaN(newGridSize) || newGridSize < 2 || newGridSize > 100) {
-        newGridSize = defaultGridSize;
-    }
-    clearGrid();
-    buildGrid(newGridSize);
-})
-
-// Iniitate screen with default grid size
-buildGrid(defaultGridSize);
 
 function clearGrid() {
     
@@ -51,17 +28,48 @@ function buildGrid(gridSize) {
             //divcol.textContent = j;
             // Add hover listener
             divcol.addEventListener('mouseover', function(event) {
-                this.classList.add(hoverClass);
+                // this is the divcol (the thing calling/initiating the function)
+                // would also be accessible via the event argument (from addEventListener callback) event.target
+                //this.classList.add('hover');
+                const rgbR = Math.floor(Math.random()*255);
+                const rgbG = Math.floor(Math.random()*255);
+                const rgbB = Math.floor(Math.random()*255);
+                this.style.backgroundColor = "rgb(" + [rgbR, rgbG, rgbB].join() + ")";
             })
             divcol.addEventListener('mouseout', function(event) {
                 const cell = this;
                 // After the time variable, you can pass any number of arguments 
                 // that you need to be available at setTimeout runtime
                 setTimeout(function() {
-                    cell.classList.remove(hoverClass);
-                }, 5000, cell)
+                    //cell.classList.remove('hover');
+                    cell.style.backgroundColor = null;
+                }, 500, cell)
             })
 
         }
     }
 }
+
+const container = document.querySelector('#container');
+const defaultGridSize = 16;
+
+// Create header
+const divhdr = document.createElement('div');
+container.appendChild(divhdr);
+// Create header button
+const hdrbtn = document.createElement('button');
+divhdr.appendChild(hdrbtn);
+hdrbtn.textContent = "Click to Adjust Grid Size";
+// Header button click action
+hdrbtn.addEventListener('click', function() {
+    let newGridSize = prompt("Grid size - inputer number between 2 and 100", defaultGridSize);
+    if(isNaN(newGridSize) || newGridSize < 2 || newGridSize > 100) {
+        newGridSize = defaultGridSize;
+    }
+    clearGrid();
+    buildGrid(newGridSize);
+})
+
+// Iniitate screen with default grid size
+buildGrid(defaultGridSize);
+
